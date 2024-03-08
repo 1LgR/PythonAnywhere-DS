5c3d27a0cd9acaac1827b0447288708e3ef54f7d
Luiz Gustavo Rocha <LuizGustavoRocha@4146c987a9a6>
2024-03-08 12:38:34 +0000

    Site PythonAnywhere

---
+++ mysite/enquetes/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("<h1>Disciplina:DSweb 2024.1 <br><br> Matricula: 20231014040008 <br><br> Aluno: Luiz Gustavo Oliveira Rocha</h1>")
