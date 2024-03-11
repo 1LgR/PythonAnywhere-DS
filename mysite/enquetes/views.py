4c8c1f910bca880d4a7898d76f2c4fdfefc29e2f
Luiz Gustavo Rocha <LuizGustavoRocha@4146c987a9a6>
2024-03-11 13:45:19 +0000

    Mudanças

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Disciplina:DSweb 2024.1 <br><br> 3° período <br><br> Matricula: 20231014040008 <br><br> Aluno: Luiz Gustavo Oliveira Rocha</h1>")
+    return HttpResponse("<h1>Disciplina:DSweb 2024.1 <br> <h2> 3° período <br><br> Matricula: 20231014040008 <br><br> Aluno: Luiz Gustavo Oliveira Rocha </h2> </h1>")
