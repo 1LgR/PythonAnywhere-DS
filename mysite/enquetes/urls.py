be87c6396829b8dc7e37a3da4d3dff94479dedb8
Luiz Gustavo Rocha <LuizGustavoRocha@4146c987a9a6>
2024-03-18 14:32:41 +0000

    aula 18/03, porém não completa para Fellipão poder ver a atividade que já foi feita

--- mysite/enquetes/urls.py
+++ mysite/enquetes/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name= 'index'),
+    path('<int:pergunta_id>/', views.detalhes, name = 'detalhes'),
+    path('<int:pergunta_id>/votacao/', views.votacao, name = 'votação'),
+    path('<int:pergunta_id>/resultado/', views.resultado, name = 'resultado'),
 ]
--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Pergunta
 
 def index(request):
-    return HttpResponse("<h1>Disciplina:DSweb 2024.1 <br> <h2> 3° período <br><br> Matricula: 20231014040008 <br><br> Aluno: Luiz Gustavo Oliveira Rocha </h2> </h1>")
+    lista = Pergunta.objects.all()
+    resultado = '<br>'.join(p.texto for p in lista)
+    return HttpResponse(
+        "<h1>Disciplina:DSweb 2024.1 <br> <h2> 3° período <br><br> Matricula: 20231014040008 <br><br> Aluno: Luiz Gustavo Oliveira Rocha </h2> </h1>"
+    )
+
+def detalhes(request, pergunta_id):
+    resultado = 'DETALHES da enquente de número %s'
+    return HttpResponse(resultado % pergunta_id)
+
+def votacao(request, pergunta_id):
+    resultado = 'VOTAÇÃO da enquente de número %s'
+    return HttpResponse(resultado % pergunta_id)
+
+def resultado(request, pergunta_id):
+    resultado = 'RESULTADO da enquente de número %s'
+    return HttpResponse(resultado % pergunta_id)
