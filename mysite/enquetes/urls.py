9eb0dbfec48b31cf9d75b1491164cea611c03c27
Luiz Gustavo Rocha <LuizGustavoRocha@4146c987a9a6>
2024-03-21 12:59:42 +0000

    Aula dia 21

--- mysite/enquetes/urls.py
+++ mysite/enquetes/urls.py
@@ -2,8 +2,12 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    # URL "/enquetes/" --> lista geral das enquetes
     path('', views.index, name= 'index'),
     path('<int:pergunta_id>/', views.detalhes, name = 'detalhes'),
+    # URL "/enquetes/55/" --> detalhes da enquete com "id" 55
     path('<int:pergunta_id>/votacao/', views.votacao, name = 'votação'),
+    # URL "/enquetes/55/votacao/" --> votacao da enquete 55
     path('<int:pergunta_id>/resultado/', views.resultado, name = 'resultado'),
+    # URL "/enquetes/55/resultado/" --> resultado da enquete 55
 ]
--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Pergunta
+from django.template import loader
 
 def index(request):
-    lista = Pergunta.objects.all()
-    resultado = '<br>'.join(p.texto for p in lista)
-    return HttpResponse(
-        "<h1>Disciplina:DSweb 2024.1 <br> <h2> 3° período <br><br> Matricula: 20231014040008 <br><br> Aluno: Luiz Gustavo Oliveira Rocha </h2> </h1>"
-    )
+    enquete = Pergunta.objects.all()
+    template = loader.get_template('enquetes/index.html')
+    contexto = {'lista_enquetes': enquete}
+    return HttpResponse(template.render(contexto, request))
 
 def detalhes(request, pergunta_id):
     resultado = 'DETALHES da enquente de número %s'
