ef0249d337fafa047b14451556f1f70a5518d357
Luiz Gustavo Rocha <LuizGustavoRocha@4146c987a9a6>
2024-03-21 13:22:16 +0000

    Usando o render

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Pergunta
-from django.template import loader
 
 def index(request):
-    enquetes = Pergunta.objects.all()
-    template = loader.get_template('enquetes/index.html')
+    enquetes = Pergunta.objects.order_by('-data_pub')
     contexto = {'lista_enquetes': enquetes}
-    return HttpResponse(template.render(contexto, request))
+    return render(request, 'enquetes/index.html', contexto)
 
 def detalhes(request, pergunta_id):
     resultado = 'DETALHES da enquente de número %s'
@@ -20,3 +18,15 @@ def votacao(request, pergunta_id):
 def resultado(request, pergunta_id):
     resultado = 'RESULTADO da enquente de número %s'
     return HttpResponse(resultado % pergunta_id)
+
+####
+## Histórico de Versões
+"""
+--> View INDEX - Versão 1
+def index(request):
+    enquetes = Pergunta.objects.all()
+    template = loader.get_template('enquetes/index.html')
+    contexto = {'lista_enquetes': enquetes}
+    return HttpResponse(template.render(contexto, request))
+
+"""
