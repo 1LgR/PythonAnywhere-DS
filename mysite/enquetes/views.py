bc064e514c75f03105904392e218aca32728773e
Luiz Gustavo Rocha <LuizGustavoRocha@4146c987a9a6>
2024-04-05 12:26:37 +0000

    Atualizações aula 04/04

--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Pergunta
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Pergunta, Alternativa
 
 def index(request):
     enquetes = Pergunta.objects.order_by('-data_pub')
@@ -13,12 +14,28 @@ def detalhes(request, pergunta_id):
     return render(request, 'enquetes/detalhes.html', contexto)
 
 def votacao(request, pergunta_id):
-    resultado = 'VOTAÇÃO da enquente de número %s'
-    return HttpResponse(resultado % pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    try:
+        id_alternativa = request.POST['escolha']
+        alt = pergunta.alternativa_set.get(pk=id_alternativa)
+    except (KeyError, Alternativa.DoesNotExist):
+        contexto = {
+            'enquete': pergunta,
+            'error': 'Você precisa selecionar uma alternativa.'
+        }
+        return render(request, 'enquetes/detalhes.html', contexto)
+    else:
+        alt.quant_votos += 1
+        alt.save()
+        return HttpResponseRedirect(reverse(
+            'enquetes:resultado', args=(pergunta.id,)
+        ))
 
 def resultado(request, pergunta_id):
-    resultado = 'RESULTADO da enquente de número %s'
-    return HttpResponse(resultado % pergunta_id)
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    contexto = {'enquete': pergunta}
+    return render(request, 'enquetes/resultado.html', contexto)
+
 
 ####
 ## Histórico de Versões
