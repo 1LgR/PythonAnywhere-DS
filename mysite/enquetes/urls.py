4ade8dfc1c9d5c5deb67338eab4ff7787e14c3a3
Luiz Gustavo Rocha <LuizGustavoRocha@4146c987a9a6>
2024-07-29 02:20:06 +0000

    atividade finalizada

--- mysite/enquetes/urls.py
+++ mysite/enquetes/urls.py
@@ -1,14 +1,9 @@
-from . import views
 from django.urls import path
+from . import views
 
 app_name = 'enquetes'
 urlpatterns = [
-    # URL "/enquetes/" --> lista geral das enquetes
-    path('', views.index, name= 'index'),
-    path('<int:pergunta_id>/', views.detalhes, name = 'detalhes'),
-    # URL "/enquetes/55/" --> detalhes da enquete com "id" 55
-    path('<int:pergunta_id>/votacao/', views.votacao, name = 'votação'),
-    # URL "/enquetes/55/votacao/" --> votacao da enquete 55
-    path('<int:pergunta_id>/resultado/', views.resultado, name = 'resultado'),
-    # URL "/enquetes/55/resultado/" --> resultado da enquete 55
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetalhesView.as_view(), name='detalhes'),
+    path('<int:pk>/resultado/', views.ResultadoView.as_view(), name='resultado'),
 ]
--- mysite/enquetes/views.py
+++ mysite/enquetes/views.py
@@ -1,40 +1,53 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Pergunta, Alternativa
+from django.views import View
 
-def index(request):
-    enquetes = Pergunta.objects.order_by('-data_pub')
-    contexto = {'lista_enquetes': enquetes}
-    return render(request, 'enquetes/index.html', contexto)
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        lista = Pergunta.objects.order_by('-data_pub')
+        contexto = {'lista_enquetes': lista}
+        return render(request, 'enquetes/index.html', contexto)
 
-def detalhes(request, pergunta_id):
-    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
-    contexto = {'enquete': pergunta}
-    return render(request, 'enquetes/detalhes.html', contexto)
+class DetalhesView(View):
+    def get(self, request, *args, **kwargs):
+        pergunta_id = kwargs['pk']
+        enquete = get_object_or_404(Pergunta, pk=pergunta_id)
+        return render(request, 'enquetes/detalhes.html', {'pergunta': enquete})
 
-def votacao(request, pergunta_id):
-    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
-    try:
-        id_alternativa = request.POST['escolha']
-        alt = pergunta.alternativa_set.get(pk=id_alternativa)
-    except (KeyError, Alternativa.DoesNotExist):
-        contexto = {
-            'enquete': pergunta,
-            'error': 'Você precisa selecionar uma alternativa.'
-        }
-        return render(request, 'enquetes/detalhes.html', contexto)
-    else:
-        alt.quant_votos += 1
-        alt.save()
-        return HttpResponseRedirect(reverse(
-            'enquetes:resultado', args=(pergunta.id,)
-        ))
+    def post(self, request, *args, **kwargs):
+        pergunta_id = kwargs['pk']
+        pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
 
-def resultado(request, pergunta_id):
-    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
-    contexto = {'enquete': pergunta}
-    return render(request, 'enquetes/resultado.html', contexto)
+        id_alternativa = request.POST.get('escolha')  # Usa get para evitar KeyError
+
+        if not id_alternativa:  # Verifica se id_alternativa está vazio
+            contexto = {
+                'pergunta': pergunta,
+                'error': 'Você precisa selecionar uma alternativa.'
+            }
+            return render(request, 'enquetes/detalhes.html', contexto)
+
+        try:
+            alt = pergunta.alternativa_set.get(pk=id_alternativa)
+        except Alternativa.DoesNotExist:
+            contexto = {
+                'pergunta': pergunta,
+                'error': 'Alternativa inválida.'
+            }
+            return render(request, 'enquetes/detalhes.html', contexto)
+        else:
+            alt.quant_votos += 1
+            alt.save()
+            return HttpResponseRedirect(reverse('enquetes:resultado', args=(pergunta.id,)))
+
+class ResultadoView(View):
+    def get(self, request, *args, **kwargs):
+        pergunta_id = kwargs['pk']
+        enquete = get_object_or_404(Pergunta, pk=pergunta_id)
+        return render(request, 'enquetes/resultado.html', {'pergunta': enquete})
+
 
 
 ####
@@ -52,4 +65,44 @@ def detalhes(request, pergunta_id):
     resultado = 'DETALHES da enquente de número %s'
     return HttpResponse(resultado % pergunta_id)
 
+def index(request):
+    enquetes = Pergunta.objects.order_by('-data_pub')
+    contexto = {'lista_enquetes': enquetes}
+    return render(request, 'enquetes/index.html', contexto)
+
+def detalhes(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    contexto = {'enquete': pergunta}
+    return render(request, 'enquetes/detalhes.html', contexto)
+
+def resultado(request, pergunta_id):
+    pergunta = get_object_or_404(Pergunta, pk = pergunta_id)
+    contexto = {'enquete': pergunta}
+    return render(request, 'enquetes/resultado.html', contexto)
+
+
+
+class DetalhesView(View):
+    def get(self, request, *args, **kwargs):
+        pergunta_id = kwargs['pk']
+        enquete = get_object_or_404(Pergunta, pk=pergunta_id)
+        return render(request, 'enquetes/detalhes.html', {'pergunta': enquete})
+
+    def post(self, request, *args, **kwargs):
+        pergunta_id = kwargs['pk']
+        pergunta = get_object_or_404(Pergunta, pk=pergunta_id)
+
+        try:
+            id_alternativa = request.POST['escolha']
+            alt = pergunta.alternativa_set.get(pk=id_alternativa)
+        except (KeyError, Alternativa.DoesNotExist):
+            contexto = {
+                'enquete': pergunta,
+                'error': 'Você precisa selecionar uma alternativa.'
+            }
+            return render(request, 'enquetes/detalhes.html', contexto)
+        else:
+            alt.quant_votos += 1
+            alt.save()
+            return HttpResponseRedirect(reverse('enquetes:resultado', args=(pergunta.id,)))
 """
